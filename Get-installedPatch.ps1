a77ef5c51e53321bd28b690d0a2dc683437a359f
jampaniharish <47940348+jampaniharish@users.noreply.github.com>
2019-05-29 12:36:05 +0530

    Add files via upload

---
+++ Get-installedPatch.ps1
@@ -0,0 +1,71 @@
+﻿<#
+.Synopsis
+   This script will get details of perticular patch installed on remote computer.
+.DESCRIPTION
+   This script will get details of perticular patch installed on remote computer, in this case I am trying to get recent emergency patch installed on remote computer.
+.EXAMPLE
+   get-content "C:\temp\Hareesh\Script\Computers.txt" | get-installedpatch
+.EXAMPLE
+   get-installedpatch -computers computer1,computer2
+.INPUTS
+   computername
+.FUNCTIONALITY
+   This cmdlet is useful to check the recent emergency patch (KB4499175 or KB4499180) is installed on remote computer or not.
+#>
+
+function get-installedpatch
+{
+[cmdletbinding()]
+param
+(
+[Parameter(
+Mandatory=$true,
+ValueFromPipeline=$true
+)]
+[string[]]$computers
+)
+Begin
+{
+    $Results = @()
+}
+    
+    Process
+    {
+        foreach ($computer in $computers)
+        {
+            if (Test-Connection -ComputerName $computer -Count 1 -ea SilentlyContinue)
+            {
+                try
+                {
+                    $hotfix = get-wmiobject win32_quickfixengineering -ComputerName $computer | where {$_.hotfixid -eq "KB4499175" -or $_.hotfixid -eq "KB4499180"} -ErrorAction stop
+                    if ($hotfix.HotfixId -eq 'KB4499175'-or $hotfix.HotfixId -eq 'KB4499180')
+                    {
+                    $results += [pscustomobject] @{
+                    ComputerName = $computer
+                    HotfixId = $hotfix.HotfixID
+                    Installdate = $hotfix.Installedon
+                    }
+                    }
+                    else
+                    {
+                    $results += [pscustomobject] @{
+                    ComputerName = $computer
+                    }
+                    }
+                }
+                catch
+                {
+                Write-output "$computer is giving exception"
+                $Computer | out-file 'C:\temp\Hareesh\Script\KB4499175_ExceptionComputers.txt' -Append
+                }
+            }
+            else
+            {
+            Write-output "$computer is not online" | Out-File "C:\temp\Hareesh\Script\KB4499175_Notonline.txt"
+            }
+        $Results | export-csv "C:\temp\Hareesh\Script\KB4499175_Installed.csv" -NoTypeInformation
+        }
+     }
+end {}
+}
+get-content "C:\temp\Hareesh\Script\Computers.txt" | get-installedpatch
---
+++ WeeklyPatchComplianceReport.ps1
@@ -0,0 +1,45 @@
+﻿
+<#
+.Synopsis
+   This script will Import csv files into excel, create pivot table, calculate the compliance percentage.
+.DESCRIPTION
+   This script is used to import csv files into excel, create pivot table, calcute the compliance percentage.
+#>
+
+# Weekly ComplianceReport Clients
+
+cd C:\temp\ComplianceReport
+
+import-Csv "All Clients - Region NA.csv" | Export-Excel "Weekly ComplianceReport Clients.xlsx" -WorksheetName "All Clients - Region NA" -AutoSize
+import-Csv "NonCompliant Clients - Region NA.csv" | Export-Excel "Weekly ComplianceReport Clients.xlsx" -WorksheetName "NonCompliant Clients -Region NA" `
+-AutoSize
+
+$data = Import-Excel "Weekly ComplianceReport Clients.xlsx" -WorksheetName "NonCompliant Clients -Region NA" -StartColumn 4 -EndColumn 4 
+$data."Ip Address" | select -Unique | Export-Excel "Weekly ComplianceReport Clients.xlsx" -WorksheetName "NonCompliant Clients -Region NA" -StartColumn 13 -AutoSize -TitleSize 11 `
+-Title "Unique IP Address"
+
+$newdata = Open-ExcelPackage "Weekly ComplianceReport Clients.xlsx"
+$nullvar = Add-WorkSheet -ExcelPackage $newdata -WorksheetName "Compliance Percentage clients"
+
+$ws = $newdata.Workbook.Worksheets["Compliance Percentage clients"]
+$cell1 = $ws.Cells["A1"]
+$cell1.Value = "All Clients - Region NA"
+$cell1.Style.Font.Bold = "true"
+$cell2 = $ws.Cells["A2"]
+$cell2.Value = "NonCompliant Clients -Region NA"
+$cell2.Style.Font.Bold = "true"
+$cell3 = $ws.Cells["A3"]
+$cell3.Value = "Compliance Percentage clients"
+$cell3.Style.Font.Bold = "true"
+
+$newdata.Workbook.Worksheets["Compliance Percentage clients"].Cells[1,2].Formula ="COUNTA('All Clients - Region NA'!A:A)"
+$newdata.Workbook.Worksheets["Compliance Percentage clients"].Cells[1,2].Style.Font.Bold = "true"                                                                                  
+$newdata.Workbook.Worksheets["Compliance Percentage clients"].Cells[2,2].Formula="COUNTA('NonCompliant Clients -Region NA'!M:M)"
+$newdata.Workbook.Worksheets["Compliance Percentage clients"].Cells[2,2].Style.Font.Bold = "true"
+$newdata.Workbook.Worksheets["Compliance Percentage clients"].Cells[3,2].Formula='1-(B2/B1)'
+$newdata.Workbook.Worksheets["Compliance Percentage clients"].Cells[3,2].Style.Font.Bold = "true"
+
+Add-PivotTable -PivotTableName "Compliance Percentage clients" -address $newdata."Compliance Percentage clients".Cells["A5"] -SourceWorkSheet $newdata."NonCompliant Clients -Region NA" `
+-PivotRows 'Plugin Name' -PivotData 'Plugin Name'
+
+Close-ExcelPackage $newdata
